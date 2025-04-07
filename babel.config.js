113040d74d4bb9e46d78016e46873150a85956b0
eeren3411 <eeren3411@gmail.com>
2025-04-07 10:45:33 +0300

    Set up basic path aliases for future use

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+	api.cache(true);
+	return {
+	  presets: ['babel-preset-expo'],
+	  plugins: [
+		[
+		  'module-resolver',
+		  {
+			extensions: ['.ts', '.tsx', '.js', '.json'],
+			alias: {
+			  '@screens': './screens',
+			  '@components': './components',
+			  '@styles': './styles',
+			  '@assets': './assets',
+			  '@firebase': './firebase',
+			},
+		  },
+		],
+	  ],
+	};
+  };
+  
